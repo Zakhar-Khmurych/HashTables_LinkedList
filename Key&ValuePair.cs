026f6e578d9509330dba60f5f0a8c070347441f5
Zakhar-Khmurych <68910550+Zakhar-Khmurych@users.noreply.github.com>
2022-02-22 10:19:21 +0200

    Create Key&ValuePair.cs

---
+++ Key&ValuePair.cs
@@ -0,0 +1,16 @@
+namespace ab3term3try1
+{
+    public class Key_ValuePair
+    {
+        public string Key { get; }
+        
+        public string Value { get; }
+
+        public Key_ValuePair(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+        
+    }
+}
